cbf2176eeee4e1839a53bb58ad196d305086b68f
GiovanePS <giovanespace@gmail.com>
2024-06-29 01:29:56 -0300

    starting change direction tower

--- main.c
+++ main.c
@@ -30,7 +30,13 @@ typedef struct {
 // Estrutura da Torre
 typedef struct {
   int x, y;
+  int direcao;
 } Torre;
+
+typedef struct {
+  EstadoJogo *jogo;
+  Torre *torre;
+} entrada_args;
 
 // Constantes do Jogo
 #define NUM_NAVES 10
@@ -129,15 +135,27 @@ void recarrega_torre(EstadoJogo *jogo, Torre *torre) {
 
 // Função para Capturar Entrada do Usuário
 void *captura_entrada(void *arg) {
-  EstadoJogo *jogo = (EstadoJogo *)arg;
-  Torre torre = {tela_largura / 2, tela_altura - 4};
+  entrada_args *args = (entrada_args *)arg;
+  EstadoJogo *jogo = (EstadoJogo *)args->jogo;
+  Torre *torre = (Torre *)args->torre;
   while (1) {
     int ch = getch();
     if (ch == ' ') {
-      torre_dispara(jogo, &torre);
-    } else if (ch == 'r') {
-      recarrega_torre(jogo, &torre);
+      torre_dispara(jogo, torre);
     }
+
+    if (ch == 'r') {
+      recarrega_torre(jogo, torre);
+    }
+
+    if (ch == 'a') {
+      torre->direcao = 1;
+    } else if (ch == 'w') {
+      torre->direcao = 2;
+    } else if (ch == 'd') {
+      torre->direcao = 3;
+    }
+
     usleep(100000); // Sleep to avoid high CPU usage
   }
   return NULL;
@@ -145,7 +163,9 @@ void *captura_entrada(void *arg) {
 
 // Função para Atualizar a Interface do Jogo
 void *atualiza_interface(void *arg) {
-  EstadoJogo *jogo = (EstadoJogo *)arg;
+  entrada_args *args = (entrada_args *)arg;
+  EstadoJogo *jogo = (EstadoJogo *)args->jogo;
+  Torre *torre = (Torre *)args->torre;
   initscr();
   noecho();
   curs_set(FALSE);
@@ -158,7 +178,7 @@ void *atualiza_interface(void *arg) {
 
     for (int i = 0; i < jogo->num_naves; i++) {
       if (jogo->naves[i].ativa) {
-        mvprintw(jogo->naves[i].y, jogo->naves[i].x, "N");
+        mvprintw(jogo->naves[i].y, jogo->naves[i].x, "<<(o)>>");
       }
     }
 
@@ -168,8 +188,19 @@ void *atualiza_interface(void *arg) {
       }
     }
 
-    // Desenha a torre maior
+    // Desenha a torre
     int torre_x = tela_largura / 2;
+    if (torre->direcao == 1) {
+      mvprintw(tela_altura - 5, torre_x - 1, " | ");
+      mvprintw(tela_altura - 5, torre_x - 1, " | ");
+    } else if (torre->direcao == 2) {
+      mvprintw(tela_altura - 5, torre_x - 1, " | ");
+      mvprintw(tela_altura - 5, torre_x - 1, " | ");
+    } else if (torre->direcao == 3) {
+      mvprintw(tela_altura - 5, torre_x - 1, " | ");
+      mvprintw(tela_altura - 5, torre_x - 1, " | ");
+    }
+
     mvprintw(tela_altura - 4, torre_x - 1, "/|\\");
     mvprintw(tela_altura - 3, torre_x - 2, "/_|_\\");
     mvprintw(tela_altura - 2, torre_x - 2, " | | ");
@@ -198,15 +229,16 @@ int main() {
   getmaxyx(stdscr, tela_altura, tela_largura);
   endwin(); // End ncurses mode so we can safely initialize game
 
-  Torre torre = {tela_largura / 2, tela_altura - 4};
+  Torre torre = {tela_largura / 2, tela_altura - 4, 2};
 
   inicializa_jogo(&jogo, &torre);
 
   pthread_t thread_naves, thread_interface, thread_entrada;
 
+  entrada_args args = {&jogo, &torre};
   pthread_create(&thread_naves, NULL, movimenta_naves, (void *)&jogo);
-  pthread_create(&thread_interface, NULL, atualiza_interface, (void *)&jogo);
-  pthread_create(&thread_entrada, NULL, captura_entrada, (void *)&jogo);
+  pthread_create(&thread_interface, NULL, atualiza_interface, (void *)&args);
+  pthread_create(&thread_entrada, NULL, captura_entrada, (void *)&args);
 
   pthread_join(thread_naves, NULL);
   pthread_join(thread_interface, NULL);
